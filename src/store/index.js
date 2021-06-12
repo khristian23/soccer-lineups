353b792920ed376f8caf90b315f9f41a963d00b1
Christian Montoya <christian.montoya@sap.com>
2021-06-11 23:56:56 -0400

    First Commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+// default src/store/index.js content:
+import { createStore } from 'vuex'
+// import example from './module-example'
+
+export default function (/* { ssrContext } */) {
+  const Store = createStore({
+    modules: {
+      // example
+    },
+
+    // enable strict mode (adds overhead!)
+    // for dev mode and --debug builds only
+    strict: process.env.DEBUGGING
+  })
+
+  return Store
+}
